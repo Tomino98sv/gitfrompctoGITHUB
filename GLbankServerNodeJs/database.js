783ccde5afa1f08fa2973692c4422e3e288be67e
Tomino98sv <tomas.pavlik@akademiasovy.sk>
2019-05-23 22:11:38 +0200

    update for server

--- GLbankServerNodeJs/database.js
+++ GLbankServerNodeJs/database.js
@@ -136,7 +136,41 @@ const getAccountInfoMeth = (login,token,accNum,callback) => {
     con.query(sql,(err,res) => {
         if(err) throw err;
         if(res.length==0){
-            callback("{Could not get any account!}",403);
+            callback("{Could not get account info!}",403);
+        }else{
+                let obj = new Object();
+                obj.id=res[0].id;
+                obj.idc=res[0].idc;
+                obj.AccNum=res[0].AccNum;
+                obj.amount=res[0].amount;
+                callback(JSON.stringify(obj),200);
+        }
+    })
+    });
+
+    }else{
+        let mess = new Object();
+        mess.message = "Wrong credintials!";
+        callback(JSON.stringify(mess),401);
+    }
+}
+
+
+const transHistoryMeth = (login,idAcc,token,callback) => {
+
+
+    if(tokens.find(
+        person => (person.login == login && person.token == token)
+        )
+    ){
+     
+    con.connect(function(err){
+    console.log("Connection to database has been estabilished"); 
+    let sql="select * from transaction where idAcc like '"+idAcc+"'"; 
+    con.query(sql,(err,res) => {
+        if(err) throw err;
+        if(res.length==0){
+            callback("{Could not get any transHistory info!}",403);
         }else{
                 callback(JSON.stringify(res),200);
         }
@@ -150,4 +184,169 @@ const getAccountInfoMeth = (login,token,accNum,callback) => {
     }
 }
 
-module.exports={checkLoginMeth,logoutMeth,userinfoMeth,getAccountsMeth};
+const cardsMeth = (login,idAcc,token,callback) => {
+
+
+    if(tokens.find(
+        person => (person.login == login && person.token == token)
+        )
+    ){
+     
+    con.connect(function(err){
+    console.log("Connection to database has been estabilished"); 
+    let sql="select * from card where ida like '"+idAcc+"'"; 
+    con.query(sql,(err,res) => {
+        if(err) throw err;
+        if(res.length==0){
+            callback("{Could not find any card!}",403);
+        }else{
+                callback(JSON.stringify(res),200);
+        }
+    })
+    });
+
+    }else{
+        let mess = new Object();
+        mess.message = "Wrong credintials!";
+        callback(JSON.stringify(mess),401);
+    }
+}
+
+const cardInfoMeth = (login,token,idCard,callback) => {
+
+
+    if(tokens.find(
+        person => (person.login == login && person.token == token)
+        )
+    ){
+     
+    con.connect(function(err){
+    console.log("Connection to database has been estabilished"); 
+    let sql="select * from card where id like '"+idCard+"'"; 
+    con.query(sql,(err,res) => {
+        if(err) throw err;
+        if(res.length==0){
+            callback("{Could not get any cardInfo!}",403);
+        }else{
+                let obj = new Object();
+                 obj.id = res[0].id;
+                 obj.ida = res[0].ida;
+                 obj.PIN = res[0].PIN;
+                 obj.expirem = res[0].expirem;
+                 obj.expirey = res[0].expirey;
+                 obj.Active = res[0].Active;
+                callback(JSON.stringify(obj),200);
+        }
+    })
+    });
+
+    }else{
+        let mess = new Object();
+        mess.message = "Wrong credintials!";
+        callback(JSON.stringify(mess),401);
+    }
+}
+
+const cardTransMeth = (login,token,idCard,callback) => {
+
+
+    if(tokens.find(
+        person => (person.login == login && person.token == token)
+        )
+    ){
+     
+    con.connect(function(err){
+    console.log("Connection to database has been estabilished"); 
+    let sql="select * from cardtrans where idCard like '"+idCard+"'"; 
+    con.query(sql,(err,res) => {
+        if(err) throw err;
+        if(res.length==0){
+            callback("{Could not get any cardTransactions!}",403);
+        }else{
+            
+                callback(JSON.stringify(res),200);
+        }
+    })
+    });
+
+    }else{
+        let mess = new Object();
+        mess.message = "Wrong credintials!";
+        callback(JSON.stringify(mess),401);
+    }
+}
+
+const changePasswordMeth = (login,token,oldpassword,newpassword,callback) => {
+
+
+    if(tokens.find(
+        person => (person.login == login && person.token == token)
+        )
+    ){
+     
+    con.connect(function(err){
+    console.log("Connection to database has been estabilished"); 
+    let sql=" UPDATE loginclient set password=MD5('"+newpassword+"')"+
+    " where loginclient.login like '"+login+"' and loginclient.password "+
+    "like MD5('"+oldpassword+"')"; 
+    con.query(sql,(err,res) => {
+        if(err) throw err;
+        if(res.affectedRows==0){
+            callback("{Could not change any password!}",403);
+        }else{
+                console.log(res.affectedRows + " record(s) updated");
+                callback("{OK}",200);
+        }
+    })
+    });
+
+    }else{
+        let mess = new Object();
+        mess.message = "Wrong credintials!";
+        callback(JSON.stringify(mess),401);
+    }
+}
+
+const blockcardMeth = (login,token,idCard,callback) => {
+
+
+    if(tokens.find(
+        person => (person.login == login && person.token == token)
+        )
+    ){
+     
+    con.connect(function(err){
+    console.log("Connection to database has been estabilished"); 
+    let sql="UPDATE card set active="+0+" where card.id="+idCard; 
+    con.query(sql,(err,res) => {
+        if(err) throw err;
+        if(res.affectedRows==0){
+            callback("{Could not block card!}",403);
+        }else{
+                console.log(res.affectedRows + " record(s) updated");
+                callback("{Blocked}",200);
+        }
+    })
+    });
+
+    }else{
+        let mess = new Object();
+        mess.message = "Wrong credintials!";
+        callback(JSON.stringify(mess),401);
+    }
+}
+
+
+module.exports={
+    checkLoginMeth,
+    logoutMeth,
+    userinfoMeth,
+    getAccountsMeth,
+    getAccountInfoMeth,
+    transHistoryMeth,
+    cardsMeth,
+    cardInfoMeth,
+    cardTransMeth,
+    changePasswordMeth,
+    blockcardMeth
+};
--- GLbankServerNodeJs/index.js
+++ GLbankServerNodeJs/index.js
@@ -58,5 +58,77 @@ app.post('/accounts', (req,res)=>{
 
 });
 
+app.post('/accInfo',(req,res)=>{
+    let login = req.body.login;
+    let token = req.body.token;
+    let accnum = req.body.accnum;
+    let callback = function(value,status){
+        res.status(status).send(value);
+    };
+    database.getAccountInfoMeth(login,token,accnum,callback);
+});
+
+app.post('/transHistory',(req,res)=>{
+    let login = req.body.login;
+    let idAcc = req.body.idAcc;
+    let token = req.body.token;
+    let callback = function(value,status){
+        res.status(status).send(value);
+    };
+    database.transHistoryMeth(login,idAcc,token,callback);
+});
+
+app.post('/cards',(req,res)=>{
+    let login = req.body.login;
+    let idAcc = req.body.idAcc;
+    let token = req.body.token;
+    let callback = function(value,status){
+        res.status(status).send(value);
+    };
+    database.cardsMeth(login,idAcc,token,callback);
+});
+
+app.post('/cardinfo',(req,res)=>{
+    let login = req.body.login;
+    let token = req.body.token;
+    let idCard = req.body.idCard;
+    let callback = function(value,status){
+        res.status(status).send(value);
+    };
+    database.cardInfoMeth(login,token,idCard,callback);
+});
+
+app.post('/cardTrans',(req,res)=>{
+    let login = req.body.login;
+    let token = req.body.token;
+    let idCard = req.body.idCard;
+    let callback = function(value,status){
+        res.status(status).send(value);
+    };
+    database.cardTransMeth(login,token,idCard,callback);
+});
+
+app.post('/changePassword',(req,res)=>{
+    let login = req.body.login;
+    let token = req.body.token;
+    let newpassword = req.body.newpassword;
+    let oldpassword = req.body.oldpassword;
+
+    let callback = function(value,status){
+        res.status(status).send(value);
+    };
+    database.changePasswordMeth(login,token,oldpassword,newpassword,callback);
+});
+
+app.post('/blockcard',(req,res)=>{
+    let login = req.body.login;
+    let token = req.body.token;
+    let idCard = req.body.idCard;
+    let callback = function(value,status){
+        res.status(status).send(value);
+    };
+    database.blockcardMeth(login,token,idCard,callback);
+});
+
 console.log("ide server");
 app.listen(3000);
